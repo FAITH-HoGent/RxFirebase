5c341e1666accc4709c7d01ab97cbdaad0826a7a
Harm De Weirdt <harm.deweirdt@hogent.be>
2020-06-17 11:37:03 +0200

    update rxjava and firebase

---
+++ app/src/test/java/be/hogent/faith/rxfirebase3/RxFirebaseRemoteTest.java
@@ -0,0 +1,72 @@
+package be.hogent.faith.rxfirebase3;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Collections;
+
+import be.hogent.faith.database.rxfirebase3.RxFirebaseRemote;
+import io.reactivex.rxjava3.observers.TestObserver;
+
+import static be.hogent.faith.rxfirebase3.RxTestUtil.ANY_TIME;
+import static be.hogent.faith.rxfirebase3.RxTestUtil.EXCEPTION;
+import static be.hogent.faith.rxfirebase3.RxTestUtil.setupTask;
+import static be.hogent.faith.rxfirebase3.RxTestUtil.testOnCompleteListener;
+import static be.hogent.faith.rxfirebase3.RxTestUtil.testOnFailureListener;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class RxFirebaseRemoteTest {
+
+    @Mock
+    private Task<Void> voidTask;
+
+    @Mock
+    private FirebaseRemoteConfig firebaseConfig;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        setupTask(voidTask);
+
+        when(firebaseConfig.fetch(ANY_TIME)).thenReturn(voidTask);
+    }
+
+    @Test
+    public void fetchRemoteConfig() {
+        TestObserver fetchTestObserver = RxFirebaseRemote
+                .fetch(firebaseConfig, ANY_TIME)
+                .test();
+
+        testOnCompleteListener.getValue().onComplete(voidTask);
+
+        verify(firebaseConfig).fetch(eq(ANY_TIME));
+
+        fetchTestObserver.assertNoErrors()
+                .assertValueSet(Collections.singletonList(voidTask))
+                .assertComplete()
+                .dispose();
+    }
+
+    @Test
+    public void fetchRemoteFailure() {
+        TestObserver fetchTestObserver = RxFirebaseRemote
+                .fetch(firebaseConfig, ANY_TIME)
+                .test();
+
+        testOnFailureListener.getValue().onFailure(EXCEPTION);
+
+        verify(firebaseConfig).fetch(eq(ANY_TIME));
+
+        fetchTestObserver.assertError(EXCEPTION)
+                .assertNotComplete()
+                .dispose();
+    }
+}
